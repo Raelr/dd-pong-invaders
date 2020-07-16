036d87e73bc2e9a512ccd22f4d7244dac870928d
jueyaye <danielju0301@gmail.com>
2020-07-16 13:41:31 +1000

    moving aim pointer

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -2,12 +2,17 @@ extends Area2D
 
 export(int) var Speed = 1000
 
-var angle
+var speed = 750
 
-func _ready():
-	angle = Vector2( cos(rotation), sin(rotation) )
-
-
+func start(pos, dir):
+	rotation = dir
+	position = pos
+	
 func _physics_process(delta):
-	position += (angle * Speed) * delta
+	position += Vector2(
+		cos(rotation) * speed * delta, 
+		sin(rotation) * speed * delta
+	)
 
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -5,6 +5,7 @@ export (bool) var is_player_one
 export (bool) var update
 export (Array) var playerTextures
 
+var Bullet = preload("res://scenes/Bullet.tscn")
 var BulletInterval = 0.3
 
 var recoilTimer
@@ -24,38 +25,26 @@ func _process(_delta):
 	if update:
 		if Engine.editor_hint:
 			texture = playerTextures[0] if (is_player_one) else playerTextures[1]
-			
-func _physics_process(_delta):
-	# mouse look
-	mousePosition = get_global_mouse_position()
-	aimPosition = mousePosition
-	bulletSpawnFrom = mousePosition
-	
-	# $PointerOne.look_at(mousePosition)
-	
-	# aimPosition = to_global($PointerOne.position)
-	
-	# Fire weapon
-	if InputEvent:
-		mousePressed()
 
-func mousePressed():
+func _input(event):
+	if event is InputEventMouseButton:
+		mousePressed(event)
+
+func mousePressed(event):
 	if canShoot:
-		shootBullet()
+		print("Mouse pressed")
+		shootBullet(event)
 		
 		# recoilTimer.start()
-		
-		canShoot = false
+		# canShoot = false
 
 
-func shootBullet():
-	bullet = $BulletOne
+func shootBullet(event):
+	var bullet = Bullet.instance()
 	
-	# bullet.position = bulletSpawnFrom
+	bullet.start(global_position, rotation)
 	
-	# bullet.add_to_group("missiles")
-	
-	get_tree().get_root().add_child(bullet)
+	get_parent().add_child(bullet)
 
 
 func onRecoilTimerStopped():
